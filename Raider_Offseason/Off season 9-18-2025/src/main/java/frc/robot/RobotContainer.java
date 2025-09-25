9013bd4a7a829c37c5836f4d0ba08a4981d81ada
Daniel6424 <coffland.d.27@knobnoster.k12.mo.us>
2025-09-25 16:51:06 -0500

    test merge in Robot con
    -changed by johnathan L

--- Raider_Offseason/Off season 9-18-2025/src/main/java/frc/robot/RobotContainer.java
+++ Raider_Offseason/Off season 9-18-2025/src/main/java/frc/robot/RobotContainer.java
@@ -20,6 +20,8 @@ import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
+//johnathan's comment
+
 public class RobotContainer {
     private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
     private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second
