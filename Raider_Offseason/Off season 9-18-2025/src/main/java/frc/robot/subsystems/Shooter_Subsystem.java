db8d6d3ebec394530069147a7b4e2dbca03430ae
Daniel6424 <coffland.d.27@knobnoster.k12.mo.us>
2025-11-20 16:14:11 -0600

    Last weeks code 11/13/25
    PID Work and a bit of logging
    Daniel.C

--- Raider_Offseason/Off season 9-18-2025/src/main/java/frc/robot/subsystems/Shooter_Subsystem.java
+++ Raider_Offseason/Off season 9-18-2025/src/main/java/frc/robot/subsystems/Shooter_Subsystem.java
@@ -21,8 +21,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-//pid code
-
+// pid code
 public class Shooter_Subsystem extends SubsystemBase {
   private DoubleSubscriber NTSpeedTop;
   private DoubleSubscriber NTSpeedBottom;
