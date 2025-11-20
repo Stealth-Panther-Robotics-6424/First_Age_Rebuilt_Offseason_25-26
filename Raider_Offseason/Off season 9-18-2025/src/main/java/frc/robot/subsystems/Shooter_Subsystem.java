98160fbb1a7fc97cc434cb6a97ed89cd49327a41
Daniel6424 <coffland.d.27@knobnoster.k12.mo.us>
2025-11-20 15:57:15 -0600

    better code
    Daniel.C

--- Raider_Offseason/Off season 9-18-2025/src/main/java/frc/robot/subsystems/Shooter_Subsystem.java
+++ Raider_Offseason/Off season 9-18-2025/src/main/java/frc/robot/subsystems/Shooter_Subsystem.java
@@ -9,6 +9,8 @@ import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 
 import com.ctre.phoenix6.SignalLogger;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.networktables.DoublePublisher;
@@ -18,6 +20,8 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+//pid code
 
 public class Shooter_Subsystem extends SubsystemBase {
   private DoubleSubscriber NTSpeedTop;
@@ -33,6 +37,9 @@ public class Shooter_Subsystem extends SubsystemBase {
   private static TalonFX topFx = new TalonFX(Constants.TOP_FX_CANID, "Upper Deck");
   private static TalonFX bottomFx = new TalonFX(Constants.BOTTOM_FX_CANID, "Upper Deck");
 
+  final VelocityVoltage top_request = new VelocityVoltage(0).withSlot(0);
+  final VelocityVoltage bottom_request = new VelocityVoltage(0).withSlot(0);
+
   /** Creates a new Shooter_Subsystem. */
   public Shooter_Subsystem() {
     NetworkTable NT = NetworkTableInstance.getDefault().getTable("shooter");
@@ -43,11 +50,28 @@ public class Shooter_Subsystem extends SubsystemBase {
     BottomSpeed = NT.getDoubleTopic("BottomSpeed").publish();
     TopSpeed.set(0.0);
     BottomSpeed.set(0.0);
+
+    // in init function, set slot 0 gains
+    var slot0Configs = new Slot0Configs();
+    slot0Configs.kS = 0.1; // Add 0.1 V output to overcome static friction
+    slot0Configs.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
+    slot0Configs.kP = 0.11; // An error of 1 rps results in 0.11 V output
+    slot0Configs.kI = 0; // no output for integrated error
+    slot0Configs.kD = 0; // no output for error derivative
+
+    topFx.getConfigurator().apply(slot0Configs);
+    bottomFx.getConfigurator().apply(slot0Configs);
   }
 
   private void setMotorPower(double power) {
     topFx.set(power);
     bottomFx.set(power);
+
+  }
+
+  private void setMotorSpeed(double speed) {
+    topFx.setControl(top_request.withVelocity(speed));
+    bottomFx.setControl(bottom_request.withVelocity(speed));
 
   }
 
@@ -85,6 +109,31 @@ public class Shooter_Subsystem extends SubsystemBase {
 
         () -> false,
         this);
+  }
+
+  public Command setVelocityCommand(double speed) {
+    return new FunctionalCommand(
+        () -> {
+          setMotorSpeed(speed);
+
+        },
+
+        () -> {
+
+        },
+
+        interrupted -> {
+          setMotorSpeed(0);
+
+        },
+
+        () -> false,
+        this);
+  }
+
+  public Command highSpeed() {
+    return setVelocityCommand(1000);
+
   }
 
   @Override
